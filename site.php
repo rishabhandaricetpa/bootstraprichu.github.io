90a37efc0f50fa57da6ec4c30ca34d36e8594528
Rebecca Risha <rebeccarisha@Administrators-Mac-mini.local>
2020-02-07 18:31:56 +0530

    first

---
+++ site.php
@@ -0,0 +1,141 @@
+<html>
+    <head> </head>
+    <link href="sty.css" type="text/css" rel="stylesheet" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    
+    <body><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<div class="menu">
+    
+  <div class="left">
+      <h1   class="animated infinite heartbeat delay-2s" style="color:white; padding-left:70px; font-size:45px;  ">Yatra ZOne</h1>
+     
+    
+  </div>
+  <div class="right">
+  <ul>
+  
+ 
+ <li> <a style="color:white;"> DESTINATIONS</a> 
+  <ul>
+      <li><a href="login.html">Amritsar</a></li>
+      <li>Dehradun</li>
+      <li>Himanchal Pradesh</li>
+      <li>Others</li>
+  </ul>
+  </li>
+ <li> <a  style="color:white;"> LONG/SHORT STAYS</a>
+
+ <ul>
+      <li><a href="boot.php">4 Dhaam yatra stays</a></li>
+      <li>Rishikesh Long stay</li>
+      <li>Adventurous stays </li>
+      <li>Haridwar short stays </li>
+      <li>Delhi NCR stays </li>
+      <li>OTHERS</li>
+
+   </ul>
+
+</li>
+ <li> <a style="color:white;"> WORK WITH US</a> 
+ <ul>
+      <li>Career</li>
+      <li>Volunteers</li>
+      <li>Host An event</li>
+   
+   </ul>
+
+
+</li>
+
+</ul>
+
+
+  </div>
+</div>
+
+
+
+
+<div class="text">
+    <h4 style="position:relative; left:250px; margin-bottom:5px;">To be present <span class="change_content"> </span></h4>
+    <h1 style="color:blue; background:rgb(192,192,192); width:60%; padding-left:250px; margin-top:5px;">TRAVEL ~ COMFORT ~ CARE </h1>
+    <h3 style="position:relative; left:250px; margin-bottom:15px;"> php is the best </h3>
+    <button style="margin-bottom:15px;" id="btnone"> LIKE</button>
+    <button id="btntwo"> SHARE</button>
+</div>
+
+<div class="cards">
+    <div class="container">
+     <div class="row" id="firstcard">
+         <div class="card">
+             <div class="card-body">
+                 <img src="https://hellotravel.gumlet.com/images/deals/360X230/-83078_84361496902675-0-.jpg?w=480&dpr=2.6" alt="" class="img-fluid rounded circle w-30 mb-3 ">
+                  </div>
+
+                </div>
+
+
+                <div class="card" id="seccard">
+             <div class="card-body">
+                 <img src="https://hellotravel.gumlet.com/images/deals/360X230/-83078_84361496902675-0-.jpg?w=480&dpr=2.6" alt="" class="img-fluid rounded circle w-30 mb-3 ">
+                  </div>
+
+                </div>
+              
+
+
+                
+                </div>
+                </div>
+</div>       
+
+<div class="footer" >
+ <div class="footer-content">
+     <h2 style="color:red; background:skyblue;">  Refrences</h2>
+     <p> On Yatra.com, you can tailor your trip from end-to-end by scouring suitable
+      flights and making your flight booking before proceeding with your hotel bookings.
+      </p>
+<div class="footer-about">
+  <p> <h1 style="font-family:cursive; color:red; background:skyblue; font-size:40px;"> ABOUT US </h1>Featured snippets, "People also ask," Images, and Videos now appear in more search results than ever before. 
+      How many of those results are yours? Quickly 
+      identify and capture the best opportunities using our Featured Snippets+ Tool for ultimate SERP domination.</p>
+  
+
+</div>
+<div class="footer-section link"></div>
+ <div class="contact-form">
+     <h2 style="color:red; background:skyblue;">Contact Form</h2>
+        <form action="action_page.php">
+
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name.."><br>
+
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name.."><br>
+
+        <label for="country">Country</label>
+        <select id="country" name="country">
+        <option value="australia">Australia</option>
+        <option value="canada">Canada</option>
+        <option value="usa">India</option>
+        </select><br>
+       
+
+        
+        <label for="subject">Comment</label>
+        <textarea id="subject" name="subject" placeholder="Write something.." style="height:100px; width:200px;"></textarea><br>
+
+        <input style="font-size:20px; color:blue;"type="submit" value="Submit">
+
+        </form>     
+
+
+</div>    
+</div>
+</div>
+
+
+
+
+</body>
+</html>
